329d79919bc9634d087c2869bb181d03d0483e2c
Sergei Kanin <k4nin-sergei@yandex.ru>
2024-04-08 19:58:25 +0300

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const placeList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
+function createCard(initialCards, deleteCardCallback) {
+  // клонировать шаблон
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
+  // установить значения вложенных элементов
+  const cardImage = cardTemplate.querySelector('.card__image');
+  const cardTitle = cardTemplate.querySelector('.card__title');
+  cardImage.src = initialCards.link;
+  cardTitle.textContent = initialCards.name;
+
+  // добавить к иконке удаления обработчик клика, по которому будет вызван переданный в аргументах колбэк
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', function() {
+    deleteCardCallback(cardElement);
+  })
+
+  return cardElement;
+}
 // @todo: Функция удаления карточки
-
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach((initialCards) => {
+  placeList.append(createCard(initialCards, deleteCard));
+})
