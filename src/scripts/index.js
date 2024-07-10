afa7ef3a73b0e6a458db66e6c3fead062ae59252
szmergon <k4nin@mail.ru>
2024-07-11 01:57:10 +0300

    1.2

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,32 @@
+import { initialCards } from "./cards";
+import "../pages/index.css";
+
+const addIcon = new URL("../images/add-icon.svg", import.meta.url);
+const avatar = new URL("../images/avatar.jpg", import.meta.url);
+const cardOne = new URL("../images/card_1.jpg", import.meta.url);
+const cardTwo = new URL("../images/card_2.jpg", import.meta.url);
+const cardThree = new URL("../images/card_3.jpg", import.meta.url);
+const closeIcon = new URL("../images/close.svg", import.meta.url);
+const deleteIcon = new URL("../images/delete-icon.svg", import.meta.url);
+const editIcon = new URL("../images/edit-icon.svg", import.meta.url);
+const likeActiveIcon = new URL("../images/like-active.svg", import.meta.url);
+const likeInactiveIcon = new URL("../images/like-inactive.svg", import.meta.url);
+const logoIcon = new URL("../images/logo.svg", import.meta.url);
+
+export const images = [
+  { name: "addIcon", link: addIcon },
+  { name: "avatar", link: avatar },
+  { name: "cardOne", link: cardOne },
+  { name: "cardTwo", link: cardTwo },
+  { name: "cardThree", link: cardThree },
+  { name: "closeIcon", link: closeIcon },
+  { name: "deleteIcon", link: deleteIcon },
+  { name: "editIcon", link: editIcon },
+  { name: "likeActiveIcon", link: likeActiveIcon },
+  { name: "likeInactiveIcon", link: likeInactiveIcon },
+  { name: "logoIcon", link: logoIcon }
+]
+
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
